409e85bbac59328be7e1e2f78cb6e41536392940
WevertonCouto <wevertoncristais@gmail.com>
2018-12-15 15:41:39 -0200

    Add empty activity (StudyActivity)

---
+++ app/src/main/java/com/example/biaeweverton/projetowb/files/StudyActivity.java
@@ -0,0 +1,15 @@
+package com.example.biaeweverton.projetowb.files;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.biaeweverton.projetowb.R;
+
+public class StudyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study);
+    }
+}
